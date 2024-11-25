70f7b8dbf5c6c1360b584464960d48e09c85f186
devAdaid <ahyeong2533@gmail.com>
2024-11-26 05:28:41 +0900

    해상도 대응

--- Assets/Scripts/Common/FixedAspectRatio..cs
+++ Assets/Scripts/Common/FixedAspectRatio..cs
@@ -1,46 +1,54 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Camera))]
 public class FixedAspectRatio : MonoBehaviour
 {
-    public float targetAspect = 16.0f / 9.0f; // 원하는 화면 비율 (16:9)
+    public float targetAspect = 16.0f / 9.0f; // 고정할 화면 비율
 
     void Update()
     {
-        // 현재 화면의 비율 계산
         float windowAspect = (float)Screen.width / (float)Screen.height;
-
-        // 비율 차이 계산
         float scaleHeight = windowAspect / targetAspect;
 
-        // 카메라 설정 조정
         Camera camera = GetComponent<Camera>();
 
-        if (scaleHeight < 1.0f)
+        if (scaleHeight < 1.0f) // 세로 레터박스 (상하 검정)
         {
-            // 화면 높이가 더 큰 경우 (Letterbox 추가)
-            Rect rect = camera.rect;
-
-            rect.width = 1.0f;
-            rect.height = scaleHeight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleHeight) / 2.0f;
-
-            camera.rect = rect;
+            float size = (1.0f - scaleHeight) / 2.0f;
+            camera.rect = new Rect(0, size, 1, scaleHeight);
         }
-        else
+        else // 가로 필러박스 (좌우 검정)
         {
-            // 화면 너비가 더 큰 경우 (Pillarbox 추가)
             float scaleWidth = 1.0f / scaleHeight;
-
-            Rect rect = camera.rect;
-
-            rect.width = scaleWidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scaleWidth) / 2.0f;
-            rect.y = 0;
-
-            camera.rect = rect;
+            float size = (1.0f - scaleWidth) / 2.0f;
+            camera.rect = new Rect(size, 0, scaleWidth, 1);
         }
+    }
+    void OnPreCull()
+    {
+        GL.Clear(true, true, Color.black); // 레터박스 영역을 강제로 검정색으로 칠함
+    }
+
+    void OnEnable()
+    {
+#if !UNITY_EDITOR
+
+        RenderPipelineManager.beginCameraRendering += RenderPipelineManager_endCameraRendering;
+#endif
+    }
+
+    void OnDisable()
+    {
+#if !UNITY_EDITOR
+
+        RenderPipelineManager.beginCameraRendering -= RenderPipelineManager_endCameraRendering;
+#endif
+
+    }
+
+    private void RenderPipelineManager_endCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        GL.Clear(true, true, Color.black);
     }
 }
