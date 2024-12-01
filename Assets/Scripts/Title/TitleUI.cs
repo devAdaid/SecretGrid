16e59d61f75f02e5c6fe29e2e836e29bc277ed86
devAdaid <ahyeong2533@gmail.com>
2024-12-01 17:57:04 +0900

    크레딧 링크 연결, CRT 효과 수정

--- Assets/Scripts/Title/TitleUI.cs
+++ Assets/Scripts/Title/TitleUI.cs
@@ -8,18 +8,18 @@ public class TitleUI : MonoBehaviour
 
     [SerializeField]
     private HeroGameButtonBase leaderboardButton;
-    
+
     [SerializeField]
     private HeroGameButtonBase creditsButton;
 
     [SerializeField]
     private HeroGameButtonBase quitButton;
-    
+
     private void Awake()
     {
         startButton.AddOnClickListener(() => SceneManager.LoadScene("Hero"));
         leaderboardButton.AddOnClickListener(() => SceneManager.LoadScene("Leaderboard"));
-        creditsButton.AddOnClickListener(() => Application.OpenURL("https://itch.io"));
+        creditsButton.AddOnClickListener(() => Application.OpenURL("https://adaid.notion.site/The-Secret-Hero-Credit-14a84d0908ea8062ad2ee51ae73888ba?pvs=4"));
         quitButton.AddOnClickListener(OnQuitButtonClicked);
     }
 
